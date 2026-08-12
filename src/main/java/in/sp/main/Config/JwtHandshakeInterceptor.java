56faef043f522100e0c309e017c3e8698c910b2d
kishor-gte <kishor@gtechnohubb.com>
2026-08-12 11:55:40 +0530

    conflict solved

--- src/main/java/in/sp/main/Config/JwtHandshakeInterceptor.java
+++ src/main/java/in/sp/main/Config/JwtHandshakeInterceptor.java
@@ -71,8 +71,12 @@ public class JwtHandshakeInterceptor implements HandshakeInterceptor {
                     .ifPresent(d -> attributes.put("authUserId", d.getId()));
             case "PROVIDER" -> providerRepository.findByEmail(email)
                     .ifPresent(p -> attributes.put("authUserId", p.getId()));
-            case "SALON" -> salonRepository.findByEmail(email)
-                    .ifPresent(s -> attributes.put("authUserId", s.getId()));
+            case "SALON" -> {
+                var s = salonRepository.findByEmail(email);
+                if (s != null) {
+                    attributes.put("authUserId", s.getId());
+                }
+            }
             default -> {
                 // Other roles still authenticate by email/role for topic ACLs
             }
